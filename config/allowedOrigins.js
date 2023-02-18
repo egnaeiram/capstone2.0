e3e494924b5f9ee093d4dd819502b31e54cbf71d
egnaeiram <114033633+egnaeiram@users.noreply.github.com>
2023-02-17 19:33:16 -0500

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
     'https://www.ryde.com/admin',
     'http://127.0.0.1:5500',
     'http://localhost:3500'
+    'http://localhost:3333'
+    
 ];
 
 module.exports = allowedOrigins;
